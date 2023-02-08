5c45d4b67927ccd6ac5e1719f704de639ee504c9
Hans Otto Wirtz <hansottowirtz@gmail.com>
2023-02-08 07:52:18 +0100

    Setup

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import { faAnchor  } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Page() {
+  return <div>hi<FontAwesomeIcon icon={faAnchor}/></div>;
+}
